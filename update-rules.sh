0c475459c82978c6ebb0dca31a7020826ac24e71
Rekey <rekey@me.com>
2026-08-05 11:29:24 +0800

    Add GitHub Actions workflow: daily build & deploy rules to Pages; fix update-rules.sh first-run mv guard

--- update-rules.sh
+++ update-rules.sh
@@ -1,10 +1,10 @@
 git clone --depth 1 -b meta https://github.com/MetaCubeX/meta-rules-dat ./meta-rules-dat-new
 git clone --depth 1 -b release https://github.com/Chocolate4U/Iran-v2ray-rules ./Iran-v2ray-rules-new
 
-mv meta-rules-dat meta-rules-dat-bak
+[ -d meta-rules-dat ] && mv meta-rules-dat meta-rules-dat-bak
 mv meta-rules-dat-new meta-rules-dat
 
-mv Iran-v2ray-rules Iran-v2ray-rules-bak
+[ -d Iran-v2ray-rules ] && mv Iran-v2ray-rules Iran-v2ray-rules-bak
 mv Iran-v2ray-rules-new Iran-v2ray-rules
 
 rm -rf Iran-v2ray-rules-bak
